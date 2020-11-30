e068cb9076738da80ae5b3df62f4fda020037b8b
Marisa You <marisayou2017@u.northwestern.edu>
2020-11-29 22:06:16 -0800

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record) {
+  const win = record.find(function(entry) {
+    return entry.result === "W"
+  })
+  return win ? win.year : undefined
+}
